b0cd61c1ca80e38fdc59d3980d0c4e2a9d0fe206
ono <f25jzzydg@me.com>
2020-04-04 06:32:42 +0900

    fix/blard_class

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
 'use strict';
 
 {
+    /**
+     * ゲームの進行を管理する
+     */
     class Board {
-        constructor(maxCardNum, suits, element) {
+        /**
+         * コンストラクタ
+         * @param int limitCardNum 
+         * @param array suitVariation 
+         * @param Object element 
+         */
+        constructor(limitCardNum, suitVariation, element) {
+            this.players = [];
+            this.isYourTurn = true;
             this.firstCard = null;
             this.secondCard = null;
-            this.maxCardNum = maxCardNum;
-            this.suits = suits;
+            this.limitCardNum = limitCardNum;
+            this.suitVariation = suitVariation;
             this.element = element;
-            this.got = [];
         }
     }
 
+
+    /**
+     * ゲームに使用するカード
+     */
     class Card {
+        /**
+         * コンストラクタ
+         * @param int num 
+         * @param string suit 
+         * @param Board board 
+         */
         constructor(num, suit, board) {
             this.num = num;
             this.suit = suit;
@@ -21,17 +41,47 @@
         }
     }
 
+    /**
+     * ゲームに参加するプレイヤ
+     */
+    class Player {
+        /**
+         * 
+         * @param string name 
+         */
+        constructor(name) {
+            this.name = name;
+            this.cards = [];
+        }
+    }
+
+    /**
+     * ゲーム開始に必要な準備をする
+     */
     function init() {
+        // ボードを作成する
         let board = new Board(6, ['club', 'diam', 'heart', 'spade'], $("#board"));
 
-        for (let i = 1; i <= board.maxCardNum; i++) {
-            board.suits.forEach(suit => {
+        // 参加プレイヤをボードにセットする
+        let you = new Player('you');
+        let rival = new Player('rival');
+        board.players[you.name] = you;
+        board.players[rival.name] = rival;
+
+        // 使用するカードをボードにセットする
+        for (let i = 1; i <= board.limitCardNum; i++) {
+            board.suitVariation.forEach(suit => {
                 let card = new Card(i, suit, board);
                 board.element.append(card.element);
             });
         }
     }
 
+    /**
+     * カードのHTML要素を生成する
+     * @param Card card 
+     * @param Board board 
+     */
     function createCardElement(card, board) {
         let front = $("<div></div>").addClass("card-front suit-" + card.suit).append(card.num);
         let back = $("<div></div>").addClass("card-back").html('CARD');
@@ -45,10 +95,19 @@
         return element;
     }
 
+    /**
+     * 引数のカードの表裏を反転する
+     * @param Card card 
+     */
     function flipCard(card) {
         card.body.toggleClass('card-open');
     }
 
+    /**
+     * 裏向きのカードを1枚めくる
+     * @param Card card 
+     * @param Board board 
+     */
     function openCard(card, board) {
         if (card.body.hasClass('card-open')) return;
 
@@ -64,11 +123,21 @@
         flipCard(card);
     }
 
+    /**
+     * プレイヤが選んだ2枚のカードが一致すればプレイヤがカードを獲得する
+     * @param Board board 
+     */
     function tryGetCards(board) {
-        if (board.firstCard.num === board.secondCard.num) {
+        if (board.firstCard.num !== board.secondCard.num) {
+            flipCard(board.firstCard);
+            flipCard(board.secondCard);
+            board.isYourTurn = !board.isYourTurn;
+        } else {
             board.firstCard.body.off();
             board.secondCard.body.off();
-            board.got.push(board.firstCard, board.secondCard);
+
+            let player = board.isYourTurn ? board.players['you'] : board.players['rival'];
+            player.cards.push(board.firstCard, board.secondCard);
         }
         board.firstCard = null;
         board.secondCard = null;
